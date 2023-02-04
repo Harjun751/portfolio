d9d9454c620600ba52a13e12627ab3d9153f0a5a
Arjun <harjun751@gmail.com>
2023-02-04 11:42:14 +0800

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Harjun751/Harjun751.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push https://github.com/Harjun751/Harjun751.github.io HEAD:main -f
 
 cd -
